f6db9f9899736d037eb7f240d461affbaa4335fd
Scregneugneuu <lartiguear@gmail.com>
2022-03-07 16:06:21 +0100

    Added q1 + q2

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import random
+
+#Question 1
+def generateBooleanTab(n):
+    tab = []
+    for i in range(n):
+        tab.append(random.choice[True, False])
+    return tab
+
+#Question 2
 def mystere(tab, n):
     i = 1
     while i  <= n and tab[i] != True:
         i += 1
     
     return i
+
 

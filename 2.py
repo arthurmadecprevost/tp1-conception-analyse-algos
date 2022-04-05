d8278ec0e797e97cb78f12a2e65e7be3aeddc21f
arthurmadecprevost <arthur.madecprevost@protonmail.com>
2022-04-05 12:17:01 +0200

    Pourquoi pas

--- 2.py
+++ 2.py
@@ -9,9 +9,35 @@ def MaxSomme1(tab):
                 sommeMax = sommeTemp
     return sommeMax
 
-def MaxSomme2:
-    i = 0
+def MaxSomme2(tab, debut, fin):
+    i, somme, mg, md = 0
+    
+    if debut = fin:
+        return max(0, tab[fin])
+    
+    milieu = pei((debut+fin)/2)
 
+    mg = 0
+    somme = 0
+
+    i = milieu
+
+    while i >= debut:
+        somme = somme + tab[i]
+        mg = max(mg, somme)
+        i = i - 1
+    
+    md = 0
+    somme = 0
+
+    i = milieu + 1
+
+    while i <= fin:
+        somme = somme + tab[i]
+        md = max(md, somme)
+        i = i + 1
+    
+    return max3(mg + md, MaxSomme2(tab, debut, milieu), MaxSomme2(tab, milieu + 1, fin))
 
 
 tab = [1,2,3]

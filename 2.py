55a50fb8594cac77b14aaca8ef2319419726ab12
Lartigue <arnaud.lartigue@etudiant.univ-rennes1.fr>
2022-04-07 16:34:56 +0200

    oui2

--- 2.py
+++ 2.py
@@ -1,3 +1,27 @@
+import random
+import threading
+from time import *
+def generateTab(n):
+    tab = []
+    neg = random.randint(0,n)
+    pos = random.randint(0,n)
+    while(pos == neg):
+            pos = random.randint(0,n)
+    for i in range(n):
+        if i == neg:
+            tab.append(random.randint(int(-(n/2)),-1))
+        elif i == pos:
+            tab.append(random.randint(1,int(n/2)))
+        else :
+            tab.append(random.choice([random.randint(int(-(n/2)),-1),random.randint(1,int(n/2))]))
+    return tab
+
+def generateTabs(n):
+    tab = []
+    for i in range(n):
+        tab.append(generateTab(n))
+    return tab
+    
 def MaxSomme1(tab):
     sommeMax = tab[0] - tab[1]
     sommeTemp = 0
@@ -12,7 +36,7 @@ def MaxSomme1(tab):
 def MaxSomme2(tab, debut, fin):
     i, somme, mg, md = 0
     
-    if debut = fin:
+    if debut == fin:
         return max(0, tab[fin])
     
     milieu = pei((debut+fin)/2)
@@ -39,6 +63,46 @@ def MaxSomme2(tab, debut, fin):
     
     return max3(mg + md, MaxSomme2(tab, debut, milieu), MaxSomme2(tab, milieu + 1, fin))
 
+def maxSomme3(tab, n):
+    p,q,i = 0
 
-tab = [1,2,3]
-MaxSomme1(tab)
+    while i<n:
+        q = max(q+tab[i],0)
+        p = max(p,q)
+        i+=1
+    return p
+
+def pei(n):
+    return int(n)
+
+def max3(a,b,c):
+    return max(a,b,c)
+
+
+n = [1000, 10000, 100000]
+l = [50, 500, 5000]
+demo_file = open('testSomme.txt','w')
+somme = 0 
+i=0
+event = threading.Event()
+
+while i < len(n) and somme<180 :
+    valN = n[i]
+    incr = l[i]
+    k=incr
+    while k < valN and somme<180:
+        tabs = generateTabs(k)
+        j = 0
+        while j < len(tabs) and somme<180:
+            t0 = time()
+            MaxSomme1(tabs[j])
+            t1 = time()
+            timeT = t1-t0
+            somme = somme + timeT
+            string = "k = "+str(k)+ " time = "+str(timeT)+"\n"
+            demo_file.write(string)
+            j = j + 1
+        k = k + incr
+        print(somme)
+    i = i+1
+demo_file.close()
